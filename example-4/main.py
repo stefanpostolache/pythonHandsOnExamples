5cb6e8cdc927690db79082be7489140582e76ca2
stefanpostolache <stefan.freya@gmail.com>
2020-09-01 21:38:53 +0100

    adding code to create colourful QR code

--- example-4/main.py
+++ example-4/main.py
@@ -1,5 +1,6 @@
 import qrcode
 import re
+import numpy as np
 
 """
 This module creates a QR code
@@ -32,6 +33,32 @@ def generateQRCodeFor(website, toLocation):
     img.save(toLocation)
 
 
+def generateQRCodeFor(website, andSavingToLocation, withBackgroundColor, andForegroundColor):
+    """generates a QR code for a given website and saves it in the project directory.
+    Raises URLError if the URL presented is not valid
+
+    Args:
+        website (str): URL of the website for which to generate QR code
+        andSavingToLocation (str): location to which to save file.
+        withBackgroundColor (str): color of the background of the QR code
+        andForegroundColor (str): color of the foreground of the QR code
+    """
+    valid = validateURL(website)
+
+    if not valid:
+        raise URLError
+
+    qr = qrcode.QRCode(version=1, box_size=10, border=4)
+
+    qr.add_data(website)
+
+    qr.make()
+
+    img = qr.make_image(fill_color=andForegroundColor, back_color=withBackgroundColor)
+
+    img.save(andSavingToLocation)
+
+
 def validateURL(url):
     """Validates if presented url is valid
 
@@ -48,9 +75,11 @@ if __name__=='__main__':
     import argparse
     argumentParser = argparse.ArgumentParser(description="generates a QR code and saves it to location")
     argumentParser.add_argument('-l','--location',type=str,default="qr.png",help="location to which to save QR")
+    argumentParser.add_argument('-b','--background-color',type=str,default="black",help="background color of the QR code")
+    argumentParser.add_argument('-f','--foreground-color',type=str,default="white",help="foreground color of the QR code")
     argumentParser.add_argument('website',help="name of the website for which to generate QR code")
     args = argumentParser.parse_args()
-    generateQRCodeFor(args.website,args.location)
+    generateQRCodeFor(args.website,args.location,args.background_color,args.foreground_color)
 
 
 
